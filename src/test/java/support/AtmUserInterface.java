9cbae7ffeb7856dfd63c009f4cf748fabc4e0ca8
roy <roy_radio@126.com>
2015-05-01 05:40:40 +0800

    chapter 12 complete

--- src/test/java/support/AtmUserInterface.java
+++ src/test/java/support/AtmUserInterface.java
@@ -4,10 +4,14 @@ import hooks.ServerHooks;
 import nicebank.Account;
 import nicebank.Teller;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.swing.*;
 import java.util.List;
 
 /**
@@ -34,12 +38,45 @@ public class AtmUserInterface implements Teller {
         webDriver.get("http://localhost:" + ServerHooks.PORT);
         WebElement input = webDriver.findElement(By.id("amount"));
         String amountString = String.valueOf(amount);
-        input.sendKeys(amountString);
+        for (int i = 0; i<amountString.length(); i++) {
+            input.sendKeys(convertToKey(amountString.charAt(i)));
+        }
+
+        ask("Ready to continue");
+    }
+
+    public void ask(String question) {
+        JOptionPane.showMessageDialog(
+                null,
+                question,
+                "Ask for response",
+                JOptionPane.PLAIN_MESSAGE);
     }
 
     public boolean isDisplaying(String message) {
-        List<WebElement> list = webDriver.findElements(
-                By.xpath("//*[contains(text(), '" + message + "')]"));
+        By locator = By.xpath("//*[contains(text(), '" + message + "')]");
+
+        WebDriverWait wait = new WebDriverWait(webDriver, 2, 30);
+        WebElement element = wait.until(
+                ExpectedConditions.presenceOfElementLocated(locator));
+
+        List<WebElement> list = webDriver.findElements(locator);
         return (list.size() > 0);
+    }
+
+    private Keys convertToKey(char digit) {
+        switch (digit){
+            case '0': return Keys.NUMPAD0;
+            case '1': return Keys.NUMPAD1;
+            case '2': return Keys.NUMPAD2;
+            case '3': return Keys.NUMPAD3;
+            case '4': return Keys.NUMPAD4;
+            case '5': return Keys.NUMPAD5;
+            case '6': return Keys.NUMPAD6;
+            case '7': return Keys.NUMPAD7;
+            case '8': return Keys.NUMPAD8;
+            case '9': return Keys.NUMPAD9;
+            default: throw new RuntimeException("Invalid keypress in test");
+        }
     }
 }
